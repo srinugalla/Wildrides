d46ca151b7fa469e866987280a7ab06f4da8194c
Srinivasarao Galla <srinu.galla@gmail.com>
2026-02-20 17:22:23 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_s8hHcC5ju', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'dte18l46cir7qd80oej4224s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7p9l3cp8t57ev3uc1ari2caare', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
