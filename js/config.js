038cf2fa7375cbc41e651c2f3ab1bd19b0701d31
Srinivasarao Galla <srinu.galla@gmail.com>
2026-02-20 16:56:16 +0000

    Update configuration for Cognito and API

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_s8hHcC5ju', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'dte18l46cir7qd80oej4224s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
